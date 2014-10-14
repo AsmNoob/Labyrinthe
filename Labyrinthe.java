13339e2f4bbee7d9aa114f077249e6a3eeb6e0aa
Gerard <gtionogu@ulb.ac.be>
2014-10-14 18:17:01 +0200

    Signature de la méthode de parsage

--- Labyrinthe.java
+++ Labyrinthe.java
@@ -1,5 +1,13 @@
 public class Labyrinthe{
 	//Attributs
-
+	private int matrice [][];//regarder si on mettrait pas un truc plus léger que des int
+	private int lignes; // hauteur
+	private int colonnes; // longueur
 	//Méthodes
+
+	//-------Parsage du fichier--------//
+	public void ParsageFichier(char[][] fichier){
+		// (1) lecture 1ère ligne 
+
+	}
 }
