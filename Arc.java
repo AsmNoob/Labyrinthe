e3dc0fcbcff091cddba7e65d2313f9c31cb6d3dc
Alexis <defontaine.alexis@gmail.com>
2014-10-16 20:49:32 +0200

    Ajout de methode permettant l'évolution des attributs List des class Arc et Node

--- Arc.java
+++ Arc.java
@@ -1,13 +1,18 @@
 import java.lang.Object;
 
 public class Arc {
-	//attr
+	//attribut
 	private List<List<int>> global_way;
+	//constructeur
 	public Arc(int pos_x, int pos_y){
+		add_way( pos_x, pos_y);
+	}
+	// Methode ajoutant un chemin de coordonée (x,y) dans l'attribut global_way
+	public static add_way(int pos_x,int pos_y){
 		List coord = Arrays.asList(pos_x,pos_y);
 		global_way.add(coord);
-	}
-	publi
 
+	}
+	//getter
 	
 }
--- Node.java
+++ Node.java
@@ -1,13 +1,21 @@
 import java.lang.Object;
 
 public class Node {
+	// attribut
 	private int pos_i,pos_j; // position de la node dans la matrice
-	private List<List<Node,Arc>>;
+	private List<List<Node,Arc>> ens_link_;
 
+	//constructeur
 	public Node(int pos_x, int pos_y){
 		pos_i = pos_x;
 		pos_j = pos_y;
 	}
 
+	// methode permettant de regrouper toutes les connexions existantes avec ce noeud.
+	public static add_link(Node node, Arc arc){
+		List link = Array.asList(node,arc);
+		ens_link_.add(link);
+	}
+	//getter
 	
 }
