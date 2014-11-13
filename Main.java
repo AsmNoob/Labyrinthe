c5beea8c584e92ce8dddc68d8de8ad9cc9bd1dca
gerard <gtionogu@ulb.ac.be>
2014-11-13 11:12:35 +0100

    [Main]: Added some Prints for better understanding

--- Main.java
+++ Main.java
@@ -5,9 +5,16 @@ public class Main{
 		System.out.println();
 		System.out.println(args[0]);
 		System.out.println();
+		System.out.println("Controle_MazeStart");
+		System.out.println();
 		Maze maze = new Maze(args[0]);
-		System.out.println("Controle_MainEnd");
+		System.out.println()
+		System.out.println("Controle_GraphStart");
 		System.out.println();
 		Graph graph = new Graph(maze.getMaze(),maze.getCoordPukkaman());
+		System.out.println();
+		System.out.println("Controle_GraphEnd");
+		System.out.println();
+		System.out.println("Controle_EndMain);
 	}
 }
