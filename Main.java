d7494c23f8d28c45d283db3b6ffe5de8692793d3
gerard <gtionogu@ulb.ac.be>
2014-11-13 12:07:14 +0100

    [Maze]: Commented all the useless prints

--- Main.java
+++ Main.java
@@ -8,13 +8,13 @@ public class Main{
 		System.out.println("Controle_MazeStart");
 		System.out.println();
 		Maze maze = new Maze(args[0]);
-		System.out.println()
+		System.out.println();
 		System.out.println("Controle_GraphStart");
 		System.out.println();
 		Graph graph = new Graph(maze.getMaze(),maze.getCoordPukkaman());
 		System.out.println();
 		System.out.println("Controle_GraphEnd");
 		System.out.println();
-		System.out.println("Controle_EndMain);
+		System.out.println("Controle_EndMain");
 	}
 }
