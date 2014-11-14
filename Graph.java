c7b350665ea9afed254944283cf8bf1b2976c99f
Alexis <defontaine.alexis@gmail.com>
2014-11-14 12:28:34 +0100

    [Graph] on ne travail qu'avec des positions crypt, matrice non carré accepté et plus de 99 a testé mais normalement ok

--- Graph.java
+++ Graph.java
@@ -6,12 +6,15 @@ import java.util.*;
 
 public class Graph {
 	//Attribut
-	private int dim; //dimention de la matrice a utiliser pour le cryptage
+	private int DIM_LINE; //dimention de la matrice 
+	private int DIM_COLUMN; //dimention de la matrice
+	private int LINE_SIZE; //dimention de la matrice 
+	private int COLUMN_SIZE; //dimention de la matrice
 	private HashMap<Integer,Node> ens_node = new HashMap<Integer,Node>();
 	private int[] DIRECTION = new int[] {-1,1,0,0};
 	private int DIRECTION_SIZE = DIRECTION.length-1;
-	private int iterrator;
-	private int optimisation;
+	private int iterrator = 0;
+	private int optimisation = 0;
 
 	// #define
 	
@@ -19,12 +22,14 @@ public class Graph {
 
 	// constructeur
 	public Graph(int[][] mat, int[] pacmanCoord){
-		dim = mat.length;
-		System.out.println("Dimension : " + dim);
-		iterrator = 0;
-		optimisation = 0;
+		DIM_LINE = mat.length;
+		DIM_COLUMN = mat[0].length;
+		LINE_SIZE = (int) Math.pow(10,Math.floor(Math.log10(DIM_LINE))+1);
+		COLUMN_SIZE = (int) Math.pow(10,Math.floor(Math.log10(DIM_COLUMN))+1);
+		int pacmanPos_crypt = pos_cryptage(pacmanCoord[0]*2+1,pacmanCoord[1]*2+1);
+
 		long begin = System.currentTimeMillis();
-		create_graph(mat,pacmanCoord[0]*2+1,pacmanCoord[1]*2+1, pacmanCoord[0]*2+1,pacmanCoord[1]*2+1, true, null); 
+		create_graph(mat,pacmanPos_crypt,pacmanPos_crypt, true, null); 
     	long step1 = System.currentTimeMillis();
 		graph_converter();
     	long step2 = System.currentTimeMillis();
@@ -81,17 +86,19 @@ public class Graph {
 
 	// test si la postion suivante est dans les bornes et autre qu'un mur.
 	//appel recursif de la fonction avec test_twoCase en valeur de controle car les murs se situent entre deux chemin, ils ne constituent pas de chemin en lui même. 
-	public int check_nextPosition(int[][] mat, int actuLine, int actuColumn, int preLine, int preColumn, int line_add, int column_add, boolean test_twoCase){
+	public int check_nextPosition(int[][] mat, int pos_crypt, int prePos_crypt, int line_add, int column_add, boolean test_twoCase){
 		//System.out.print(" actuLine: " );System.out.print(actuLine);System.out.print(" actuColumn: " );System.out.print(actuColumn);System.out.print(" preLine: " );System.out.print(preLine);System.out.print(" preColumn: " );System.out.print(preColumn);System.out.print(" line_add: " );System.out.print(line_add);System.out.print(" column_add: " );System.out.println(column_add);
-		int newLine = actuLine+line_add; int newColumn = actuColumn+column_add;
+		int newPos_crypt = modif_posCrypt(pos_crypt,line_add,column_add);
+		int[] newPositions = pos_decryptage(newPos_crypt);
+
 		//si on est dans le cas ou l'element vaut la sortie il ne faut pas faire d'appel récursif puisque pour revenir dans les positions courrante et non les murs
-		if (mat[actuLine][actuColumn] == 4) {test_twoCase = true;}
-		if (( (newLine >= 0) && (newLine < dim) && (newColumn>= 0) && (newColumn < dim) && (newLine != preLine || newColumn!= preColumn)) || (preLine == actuLine && preColumn == actuColumn)) {
-			int elem = mat[newLine][newColumn];
+		if (valueMat(mat, pos_crypt) == 4) {test_twoCase = true;}
+		if (( (newPositions[0] >= 0) && (newPositions[0] < DIM_LINE) && (newPositions[1]>= 0) && (newPositions[1] < DIM_COLUMN) && (newPos_crypt != prePos_crypt)) || (pos_crypt == prePos_crypt)) {
+			int elem = mat[newPositions[0]][newPositions[1]];
 			// mur = -1
 			if (elem == 4) {return 1;}
 			if (elem != -1) {
-				if (!test_twoCase){return check_nextPosition(mat, newLine, newColumn, preLine, preColumn,line_add,column_add,true);}
+				if (!test_twoCase){return check_nextPosition(mat,newPos_crypt,prePos_crypt, line_add,column_add,true);}
 				else {return 2;}
 			}
 		}
@@ -134,22 +141,25 @@ public class Graph {
 	}
 	// cryptage de la position i,j
 	public int pos_cryptage(int i, int j){
-		int pos = 10000+(i*100)+j;
+		int pos = (LINE_SIZE*COLUMN_SIZE)+(i*LINE_SIZE)+j;
 		return pos;
+	}
+	public int modif_posCrypt(int pos_crypt, int addLine, int addColumn){
+		return pos_cryptage(pos_decryptage(pos_crypt)[0]+addLine,pos_decryptage(pos_crypt)[1]+addColumn);
 	}
 
 	// decryptage de la position 
 	public int[] pos_decryptage( int pos_crypt){
-		int[] position= new int[] {pos_crypt %100,(pos_crypt-10000-(pos_crypt%100))/100};
+		int[] position= new int[] {(pos_crypt-LINE_SIZE*COLUMN_SIZE-(pos_crypt%COLUMN_SIZE))/COLUMN_SIZE,pos_crypt %COLUMN_SIZE};
 		return position; 
 	}
 
 	// detect si la position actuelle est une node et renvois une liste d'entier contenant les directions possible suivante.
-	public int[] detect_isNode(int[][] mat, int actuLine, int actuColumn, int preLine, int preColumn){
+	public int[] detect_isNode(int[][] mat, int pos_crypt, int prePos_crypt){
 		int j = DIRECTION_SIZE;
 		int[] data_direction = new int[5];// 4 premier entier -0,1- correspond au direction possible ou non le 5eme si il est >1 nous indiques que c'est une node.
 		for (int i = 0; i<= DIRECTION_SIZE; i++){
-			int test = check_nextPosition(mat, actuLine, actuColumn, preLine, preColumn,DIRECTION[i],DIRECTION[j],false);
+			int test = check_nextPosition(mat, pos_crypt, prePos_crypt,DIRECTION[i],DIRECTION[j],false);
 
 			data_direction[i] = test;
 			if (test>0){data_direction[DIRECTION_SIZE+1]+=1;}
@@ -245,22 +255,26 @@ public class Graph {
 		}
 	}
 
+	public int valueMat( int[][] mat, int pos_crypt){
+		int[] positions = pos_decryptage(pos_crypt);
+		return mat[positions[0]][positions[1]];
+	}
 	// parcours en backtraking du labyrinthe créant a chaque intersection de chemin une node - un sommet-.  
-	public void create_graph(int[][] mat, int actuLine, int actuColumn, int preLine, int preColumn, boolean isNode,Arc current_arc){
+	public void create_graph(int[][] mat, int pos_crypt, int prePos_crypt,boolean isNode,Arc current_arc){
 		//actuLine,j position actuel, preLine,preColumn position precedente
 		/*System.out.print("Controle_Create || prePos --> ");System.out.print(preLine);System.out.print(preColumn);
 		System.out.print(" || actuPos --> ");System.out.print(actuLine);System.out.print(actuColumn);
 		System.out.print(" || isNode --> ");System.out.println(isNode);
 		*/
 		Node current_node = null;
-		int pos_crypt= pos_cryptage(actuLine,actuColumn);
+		//int pos_crypt= pos_cryptage(actuLine,actuColumn);
 		iterrator++;
 		int nb_testDirection = 0;
 		if (!ens_node.containsKey(pos_crypt)){
-			int[] data_direction = detect_isNode(mat,actuLine,actuColumn,preLine,preColumn);
+			int[] data_direction = detect_isNode(mat, pos_crypt, prePos_crypt);
 
-			if (data_direction[DIRECTION_SIZE+1]>1 || mat[actuLine][actuColumn]>0)  { 
-				current_node = select_currentNode(mat[actuLine][actuColumn],pos_crypt);
+			if (data_direction[DIRECTION_SIZE+1]>1 || valueMat(mat, pos_crypt)>0)  { 
+				current_node = select_currentNode(valueMat(mat, pos_crypt),pos_crypt);
 				if (current_arc != null) {
 					end_Arc(current_arc,current_node,pos_crypt);
 					update_nodeLink(current_arc); 
@@ -273,8 +287,9 @@ public class Graph {
 			for (int i = 0; i <= DIRECTION_SIZE; i++) {
 				if (data_direction[i] >= 1) {
 					if (isNode) { current_arc = start_Arc(pos_crypt,current_node);}
-					int newLine = actuLine+(DIRECTION[i]*data_direction[i]); int newColumn = actuColumn+(DIRECTION[DIRECTION_SIZE-i]*data_direction[i]);
-					create_graph(mat,newLine,newColumn,actuLine,actuColumn,isNode,current_arc);
+					
+					int newPos_crypt = modif_posCrypt(pos_crypt,(DIRECTION[i]*data_direction[i]),(DIRECTION[DIRECTION_SIZE-i]*data_direction[i]));
+					create_graph(mat,newPos_crypt, pos_crypt,isNode,current_arc);
 					nb_testDirection++;
 				}
 				if (nb_testDirection==data_direction[DIRECTION_SIZE+1] ) {break;}
